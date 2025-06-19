a1717b5a0fdbdef03dda1dccd4ee29cad411b881
Marcelo Tapia <marcelotapia1954@gmail.com>
2025-06-19 00:21:07 -0400

    Corrige configuración de Vite para compilación

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  root: '.', // raíz del proyecto (opcional si ya estás en la raíz)
+  publicDir: 'public', // carpeta pública (ya por defecto, pero lo hacemos explícito)
+  build: {
+    outDir: 'dist', // salida de build
+  },
 });
