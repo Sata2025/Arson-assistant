2b0a618e9dca4cd138a11597fb1bb63fc61a7cc6
Marcelo Tapia <marcelotapia1954@gmail.com>
2025-06-18 21:37:00 -0400

    Corrijo ruta de entrada a index.html para Vite

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 }
+  root: '.', // raíz del proyecto
+  publicDir: 'public', // carpeta donde está el index.html
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: path.resolve(__dirname, 'public/index.html'),
+    },
+  },
 });
