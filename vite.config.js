e2a41af2f1ad23a2ed9d48c1bdf87f3fd0050114
Marcelo Tapia <marcelotapia1954@gmail.com>
2025-06-18 23:03:33 -0400

    Confirma export default LayoutConMenuFinal

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  root: '.', // raíz del proyecto
-  publicDir: 'public', // carpeta donde está el index.html
+  root: '.',
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      input: path.resolve(__dirname, 'public/index.html'),
-    },
   },
 });
